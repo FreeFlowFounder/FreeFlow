a0fb80249aea15682ff8b0aee3a3198eaa4e4b02
Travis <founder@getfreeflow.org>
2025-07-09 14:39:29 -0400

    Prepping for mainnet release

--- client/src/lib/contract-config.ts
+++ client/src/lib/contract-config.ts
@@ -12,23 +12,23 @@ interface ContractAddresses {
 export const CONTRACTS: ContractAddresses = {
   FLW: {
     testnet: "0xC474568411B2335C594B0CeE539F9f74C4103B18",
-    mainnet: "0xYourMainnetFLWTokenAddress",
+    mainnet: "0x84Ae3089F87CC1baa19586bB4C4059c7d24c648E",
   },
   USDC: {
     testnet: "0xf175520C52418dfE19C8098071a252da48Cd1C19",
-    mainnet: "0xYourMainnetUSDCTokenAddress",
+    mainnet: "0x833589fCD6eDb6E08f4c7C32D4f71b54BdA02913",
   },  
   ValidatorStaking: {
     testnet: "0xea80a4fe6e6493e96f84fada2bde6313f5fbb280",
-    mainnet: "0xYourMainnetStakingAddress",
+    mainnet: "0xd2df0cb6abf82925beadddcad517a4553be87f29",
   },
   CampaignFactory: {
     testnet: "0xb7fcfdff9e396f1c0c7bea1a3d6acae5116bf763",
-    mainnet: "0xYourMainnetFactoryAddress",
+    mainnet: "0xb76bd8906bcd73f03af1e0ff083fc1d5e59a7cb6",
   },
   FeeDistributor: {
     testnet: "0xd0875f419267bf80927160a7324c1dc94fbab269",
-    mainnet: "0xYourMainnetFeeDistributorAddress",
+    mainnet: "0xa2b0ca15ff516be34222286d54152866880b8a0d",
   },
 };
 
